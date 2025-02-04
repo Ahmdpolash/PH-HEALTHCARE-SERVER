a82a8bb18735dc2ee0b87cc7fddd1de28f122031
Ahmdpolash <ahmedpolash732@gmail.com>
2025-02-04 11:17:51 +0600

    Applying Pagination to Results

--- src/app/modules/Admin/admin.controller.ts
+++ src/app/modules/Admin/admin.controller.ts
@@ -1,19 +1,13 @@
 import { Request, Response } from "express";
 import { AdminServices } from "./admin.service";
 import pick from "../../../shared/pick";
-
-
+import { adminFilterableFields } from "./admin.constant";
 
 const getAllAdmin = async (req: Request, res: Response) => {
-
-
-
   try {
-
-    
-
-    const filters = pick(req.query, [ 'name ','email','searchTerm'])
-    const result = await AdminServices.getAllAdminFromDb(filters);
+    const filters = pick(req.query, adminFilterableFields);
+    const options = pick(req.query, ['limit', 'page', 'sortBy', 'sortOrder'])
+    const result = await AdminServices.getAllAdminFromDb(filters, options);
 
     res.status(200).json({
       success: true,
@@ -24,11 +18,8 @@ const getAllAdmin = async (req: Request, res: Response) => {
     res.status(500).json({
       success: false,
       message: "Something went wrong",
-     
     });
   }
 };
 
-
-
-export const AdminControllers = {getAllAdmin}
+export const AdminControllers = { getAllAdmin };
--- src/app/modules/Admin/admin.service.ts
+++ src/app/modules/Admin/admin.service.ts
@@ -1,19 +1,18 @@
 import { Prisma, PrismaClient } from "@prisma/client";
+import { adminSearchAbleFields } from "./admin.constant";
 
 const prisma = new PrismaClient();
 
-const getAllAdminFromDb = async (params: any) => {
+const getAllAdminFromDb = async (params: any, options: any) => {
   const { searchTerm, ...filterData } = params;
+  const { limit, page } = options;
+  console.log(filterData);
 
-  console.log(filterData)
-  
   const andConditions: Prisma.AdminWhereInput[] = [];
-
-  const seearchableFields = ["name", "email"];
 
   if (params.searchTerm) {
     andConditions.push({
-      OR: seearchableFields.map((field) => ({
+      OR: adminSearchAbleFields.map((field) => ({
         [field]: {
           contains: params.searchTerm,
           mode: "insensitive",
@@ -32,11 +31,12 @@ const getAllAdminFromDb = async (params: any) => {
     });
   }
 
-  
   const whereConditions: Prisma.AdminWhereInput = { AND: andConditions };
 
   const result = await prisma.admin.findMany({
     where: whereConditions,
+    skip: (Number(page) - 1) * limit,
+    take: Number(limit)
   });
 
   return result;
